3a3370673f2f64349ab803418bffeb64a3a147fc
Jerry Andriam <jerryandriam@gmail.com>
2024-11-20 17:13:45 +0300

    login page user

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -5,6 +5,7 @@ import { AnimatePresence } from 'framer-motion'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import Homepage from './pages/user/Homepage'
+import Login from './pages/user/Login'
 
 function App() {
   const [loading, setLoading] = useState(false)
@@ -29,6 +30,7 @@ function App() {
           <BrowserRouter>
             <Routes>
               <Route path="/" element={<Homepage />} />
+              <Route path="/login" element={<Login />} />
             </Routes>
           </BrowserRouter>
 
--- front/src/pages/user/Homepage.jsx
+++ front/src/pages/user/Homepage.jsx
@@ -1,33 +1,97 @@
 import { motion } from "framer-motion"
 import HMenu from "../../components/user/HMenu"
-
 import imgPromo from "../../assets/img-promo.png"
 import { Button } from "primereact/button"
+import { useNavigate } from "react-router-dom"
 
 const Homepage = () => {
+    const navigate = useNavigate()
+
+    const variants = {
+        hidden: { opacity: 0, y: 50 },
+        visible: (customDelay = 0) => ({
+            opacity: 1,
+            y: 0,
+            transition: { duration: 0.8, delay: customDelay },
+        }),
+    }
+
+    const imageVariants = {
+        hidden: { opacity: 0, x: 50 },
+        visible: { opacity: 1, x: 0, transition: { duration: 0.8, delay: 0.5 } },
+    }
+
     return (
-        <motion.main className="custom-bg-home h-[100vh] overflow-hidden">
+        <motion.main
+            className="custom-bg-home h-[100vh] overflow-hidden"
+            initial="hidden"
+            animate="visible"
+        >
             <HMenu />
 
-            <section className="mt-20 flex flex-row justify-between mx-44">
+            <motion.section
+                className="mt-20 flex flex-row justify-between mx-44"
+                initial="hidden"
+                animate="visible"
+            >
                 <div className="-mt-3">
-                    <h1 className="text-white text-3xl font-poppins">Bienvenue sur <span className="font-courgette text-[3rem]">Eventure</span>,</h1>
-                    <p className="text-xl text-white -mt-4 italic">Là où tous les événements se rassemblent !</p>
-                    <p className="text-[1.1em] text-white w-[74%] mt-10">Découvrez les événements près de chez vous et au-delà ! Notre plateforme vous connecte à une multitude d'activités, des conférences inspirantes aux fêtes inoubliables. Trouvez, partagez et participez facilement aux moments qui comptent.</p>
+                    <motion.h1
+                        className="text-white text-3xl font-poppins"
+                        variants={variants}
+                        custom={0.3}
+                    >
+                        Bienvenue sur{" "}
+                        <span className="font-courgette text-[3rem]">Eventure</span>,
+                    </motion.h1>
+                    <motion.p
+                        className="text-xl text-white -mt-4 italic"
+                        variants={variants}
+                        custom={0.4}
+                    >
+                        Là où tous les événements se rassemblent !
+                    </motion.p>
+                    <motion.p
+                        className="text-[1.1em] text-white w-[74%] mt-10"
+                        variants={variants}
+                        custom={0.5}
+                    >
+                        Découvrez les événements près de chez vous et au-delà ! Notre
+                        plateforme vous connecte à une multitude d'activités, des
+                        conférences inspirantes aux fêtes inoubliables. Trouvez, partagez
+                        et participez facilement aux moments qui comptent.
+                    </motion.p>
 
-                    <Button label="Commencer" icon="pi pi-arrow-right" className="bg-blackCustom font-poppins text-sm border border-none outline outline-none py-3 px-10 rounded-3xl text-white mt-7" />
+                    <motion.div
+                        variants={variants}
+                        custom={0.6}
+                    >
+                        <Button
+                            label="Commencer"
+                            icon="pi pi-arrow-right"
+                            className="bg-blackCustom font-poppins text-sm border-none py-3 px-10 rounded-3xl text-white mt-7"
+                            onClick={() => navigate('/login')}
+                        />
+                    </motion.div>
                 </div>
 
-                <div>
-                    <img src={imgPromo} alt="Promo" className="w-[21rem] h-[27rem] -mt-16" />
-                </div>
+                <motion.div variants={imageVariants}>
+                    <img
+                        src={imgPromo}
+                        alt="Promo"
+                        className="w-[21rem] h-[27rem] -mt-16"
+                    />
+                </motion.div>
+            </motion.section>
 
-            </section>
-
-            <footer className="flex flex-row justify-end me-4 mt-12">
-                <span className="text-white text-[0.65em]">Copyright 2024 - Tous droits réservés</span>
-            </footer>
-
+            <motion.footer
+                className="flex flex-row justify-end me-4 mt-12"
+                variants={variants}
+                custom={0.7}
+            >
+                <span className="text-white text-[0.65em]">
+                    Copyright 2024 - Tous droits réservés
+                </span>
+            </motion.footer>
         </motion.main>
     )
 }
