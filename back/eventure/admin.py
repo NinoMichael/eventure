8155bebd3d112b48affcb04af0489a4ae6e80770
Mirindra <mirindraandriamaroson@gmail.com>
2024-12-12 12:22:57 +0300

    configuration views.py, admin.py, urls.py, serializers.py

--- back/eventure/admin.py
+++ back/eventure/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import (
+    Utilisateur, Organisateur, Lieu, TypeEvenement, Evenement,
+    Inscription, Notification, Rappel, Programme, Espace, Message
+)
 
 # Register your models here.
+
+admin.site.register(Utilisateur)
+admin.site.register(Organisateur)
+admin.site.register(Lieu)
+admin.site.register(TypeEvenement)
+admin.site.register(Evenement)
+admin.site.register(Inscription)
+admin.site.register(Notification)
+admin.site.register(Rappel)
+admin.site.register(Programme)
+admin.site.register(Espace)
+admin.site.register(Message)
